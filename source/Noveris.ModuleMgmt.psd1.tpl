73896c01018a29ebd4b528687989f8e6fb66af5b
Jesse.Reichman <Jesse.Reichman@noveris.com.au>
2020-11-19 14:30:11 +1030

    Functions for PSGallery use and RequiredModules.
    Added function to install RequiredModules entries from
    a module manifest to support publishing of modules.
    Added function to set up powershell gallery for use locally.

--- source/Noveris.ModuleMgmt.psd1.tpl
+++ source/Noveris.ModuleMgmt.psd1.tpl
@@ -70,7 +70,9 @@
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport = @(
-        'Install-PSModuleWithSpec'
+        'Install-PSModuleWithSpec',
+        'Install-PSModuleFromManifest',
+        'Use-PowerShellGallery'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- source/Noveris.ModuleMgmt/Noveris.ModuleMgmt.psm1
+++ source/Noveris.ModuleMgmt/Noveris.ModuleMgmt.psm1
@@ -9,6 +9,130 @@ Set-StrictMode -Version 2
 
 <#
 #>
+Function Use-PowerShellGallery
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("AllUsers", "CurrentUser")]
+        [string]$Scope = "CurrentUser"
+    )
+
+    process
+    {
+        Write-Verbose "Installing Nuget package provider"
+        if ($PSCmdlet.ShouldProcess("Nuget Provider", "Update"))
+        {
+            Write-Verbose "Attempting nuget provider update"
+            try {
+                # Set TLS support to 1.1 and 1.2 explicitly
+                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12 -bor [System.Net.SecurityProtocolType]::Tls11
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false -Scope $Scope
+            } catch {
+                Write-Warning "Couldn't install nuget package provider"
+            }
+        }
+
+        Write-Verbose "Trusting PSGallery"
+        if ($PSCmdlet.ShouldProcess("PSGallery Repository", "Trust"))
+        {
+            try {
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+            } catch {
+                Write-Verbose ("Set-PSRepository for PSGallery failed: " + $_)
+            }
+        }
+    }
+}
+
+<#
+#>
+Function Install-PSModuleFromManifest
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ManifestPath,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("AllUsers", "CurrentUser")]
+        [string]$Scope = "CurrentUser",
+
+        [Parameter(Mandatory=$false)]
+        [switch]$Force = $false
+    )
+
+    process
+    {
+        Write-Verbose "Reading content from manifest: $ManifestPath"
+        $content = Get-Content -Encoding UTF8 $ManifestPath | Out-String
+
+        # Convert to a script block and execute
+        Write-Verbose "Converting to manifest object"
+        $block = [ScriptBlock]::Create($content)
+        $meta = & $block | Select-Object -First 1
+
+        # Check that we have received a hashtable from the manifest file
+        if ($null -eq $meta -or $meta.GetType().FullName -ne "System.Collections.Hashtable")
+        {
+            Write-Error "Invalid type returned from manifest file"
+            return
+        }
+
+        # Check if we have any required modules to install
+        if ($meta.Keys -notcontains "RequiredModules")
+        {
+            Write-Verbose "No required modules to install or section missing"
+            return
+        }
+
+        $meta["RequiredModules"] | ForEach-Object {
+            $module = $_
+
+            # Attempt best effort to continue if there is an invalid entry
+            if ($null -eq $module -or $module.GetType().FullName -ne "System.Collections.Hashtable")
+            {
+                Write-Warning "Missing or invalid type in RequiredModules"
+                return
+            }
+
+            # ModuleName is mandatory in the entry
+            if ($module.Keys -notcontains "ModuleName")
+            {
+                Write-Warning "Missing module name in RequiredModules entry"
+                return
+            }
+
+            # Translate keys from the RequiredModules entry to parameters for
+            # Install-Module
+            $installParams = @{
+                Scope = $Scope
+                Force = $Force
+            }
+
+            ("ModuleName", "Name"),
+                ("RequiredVersion", "RequiredVersion"),
+                ("MaximumVersion", "MaximumVersion"),
+                ("ModuleVersion", "MinimumVersion") | ForEach-Object {
+                    if ($module.Keys -contains $_[0])
+                    {
+                        $installParams[$_[1]] = $module[$_[0]]
+                    }
+                }
+
+            # Install the module
+            Write-Verbose ("Installing module: {0}" -f $installParams["Name"])
+            if ($PSCmdlet.ShouldProcess($installParams["Name"], "Install"))
+            {
+                Install-Module @installParams
+            }
+        }
+    }
+}
+
+<#
+#>
 Function Select-ModuleVersionMatches
 {
     [OutputType("System.String")]
